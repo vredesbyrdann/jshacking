4387994a3f5445057fc7e0378eaa67996db44cb9
vredesbyrdann <vbyrdann@gmail.com>
2013-11-27 16:03:44 -0500

    Fixed test.suite.js structure...

--- test.suite.js
+++ test.suite.js
@@ -2,13 +2,15 @@
 
 	var queue = [], paused = false, results;
 
-	this.test = function test(name, fn) {
-		queue.push(function queueExec() {
-			results = document.getElementById("results");
-			results = assert(true, name).appendChild(document.createElement("ul"));
-			fn();
-		});
-		runTest();
+	this.assert = function assert(bool, desc) {
+		var li = document.createElement("li");
+		li.className = bool ? "passed" : "failed";
+		li.appendChild(document.createTextNode(desc));
+		results.appendChild(li);
+		if (!bool) {
+			li.parentNode.parentNode.className = "failed";
+		}
+		return li;
 	};
 
 	this.pause = function pause() {
@@ -29,15 +31,13 @@
 		}
 	}
 
-	this.assert = function assert(bool, desc) {
-		var li = document.createElement("li");
-		li.className = bool ? "passed" : "failed";
-		li.appendChild(document.createTextNode(desc));
-		results.appendChild(li);
-		if (!bool) {
-			li.parentNode.parentNode.className = "failed";
-		}
-		return li;
+	this.test = function test(name, fn) {
+		queue.push(function queueExec() {
+			results = document.getElementById("results");
+			results = assert(true, name).appendChild(document.createElement("ul"));
+			fn();
+		});
+		runTest();
 	};
 
 })();
